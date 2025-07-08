7b469590a6438d149bd0199f4983894b6114feac
riyank036 <riyankpatel036@gmail.com>
2025-07-08 14:49:49 +0530

    add new todo after previous todo

--- 15-Todo-ContextAPI-Local/src/App.jsx
+++ 15-Todo-ContextAPI-Local/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [todos, setTodos] = useState([]);
   
   const addTodo = (todo) => {
-   setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])    
+   setTodos((prev) => [...prev, {id: Date.now(), ...todo}])    
   }
 
   const updateTodo = (id, todo) => {
